e06d59c844d1b90745ba644a8d221453aa4b4104
Devitar <46533796+Devitar@users.noreply.github.com>
2019-05-11 15:18:24 -0600

    Updates

---
+++ precache-manifest.f4b02432ff2c07f65fb30d8280a73e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9cb9f0da1a4463f70926d36d9e4207",
+    "url": "/E-commerce-Store/index.html"
+  },
+  {
+    "revision": "b99945ec6f293e84f953",
+    "url": "/E-commerce-Store/static/css/main.71648e56.chunk.css"
+  },
+  {
+    "revision": "d5e54d2c6f057485325b",
+    "url": "/E-commerce-Store/static/js/2.6fa7167c.chunk.js"
+  },
+  {
+    "revision": "b99945ec6f293e84f953",
+    "url": "/E-commerce-Store/static/js/main.d301d1ed.chunk.js"
+  },
+  {
+    "revision": "6a991f80a15e5e971941",
+    "url": "/E-commerce-Store/static/js/runtime~main.a36dc4c8.js"
+  }
+]);
